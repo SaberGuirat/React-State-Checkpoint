3d2d451c8af1f9eabdd63e6c9e6b702b28c1ff8b
saber guirat <saber.guirat92@gmail.com>
2020-10-07 17:39:33 +0200

    chekpoint completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Button, Card } from "react-bootstrap";
+import Profile from "./Assets/profilPhoto.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  state = {
+    imgSrc: Profile,
+    fullName: "Saber Guirat",
+    profession: "Student",
+    bio:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis.",
+    show: false,
+    date: new Date(),
+  };
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+  handleClick = () =>
+    this.state.show
+      ? this.setState({ show: false })
+      : this.setState({ show: true });
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+  render() {
+    if (this.state.show) {
+      return (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Card
+            style={{
+              width: "18rem",
+              background: "rgba(0,0,0,0.5)",
+              color: "white",
+            }}
+          >
+            <Card.Img
+              variant="top"
+              src={this.state.imgSrc}
+              style={{ height: 320 }}
+            />
+            <Card.Body>
+              <Card.Title>{this.state.fullName}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                {this.state.profession}
+              </Card.Subtitle>
+              <Card.Text>{this.state.bio}</Card.Text>
+              <Button variant="danger" onClick={this.handleClick}>
+                Hide
+              </Button>
+            </Card.Body>
+            <Card.Header style={{ background: "rgba(0,0,0,0.6)" }}>
+              It is {this.state.date.toLocaleTimeString()}.
+            </Card.Header>
+          </Card>
+        </div>
+      );
+    } else {
+      return (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Button variant="primary" size="lg" onClick={this.handleClick}>
+            Show
+          </Button>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
